7ad47c234fd810d1388eaca87c9179dbd4879b95
Ari Urkullu Villanueva <ari.urkullu@gmail.com>
2025-12-03 09:42:01 +0100

    multi-language

--- projects/multi-language/src/app/app.config.ts
+++ projects/multi-language/src/app/app.config.ts
@@ -1,45 +1,4 @@
-/*import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
-};*/
-
-/*import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-
-export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, './i18n/', '.json');
-}
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-  importProvidersFrom([
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: createTranslateLoader,
-        deps: [HttpClient]
-      },
-      defaultLanguage:'en',
-    })
-  ]),
-  provideRouter(routes),
-  provideHttpClient(withInterceptorsFromDi())]
-};*/
-
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
@@ -50,6 +9,7 @@ import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
@@ -57,7 +17,7 @@ export const appConfig: ApplicationConfig = {
     // Configuración moderna de ngx-translate
     provideTranslateService({
       lang: 'en',          // idioma inicial
-      fallbackLang: 'en',  // idioma de reserva
+      fallbackLang: 'es',  // idioma de reserva
       loader: provideTranslateHttpLoader({
         prefix: './i18n/', // carpeta donde tienes en.json, es.json, eu.json
         suffix: '.json'
